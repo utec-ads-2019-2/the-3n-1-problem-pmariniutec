8a11d512a1a3e5794b29d0dd2f6d4fb000f0f77c
pieromarini <piero.marini21@gmail.com>
2019-08-21 19:55:54 -0500

    Solution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
-using namespace std;
+int main() {
+  unsigned int i, j, length, maxLength;
+  while(std::cin >> i >> j){
 
-int main(int argc, char *argv[]) {
-    return 0;
+	unsigned int i2, j2;
+
+	j2 = i <= j ? j : i;
+	i2 = i <= j ? i : j;
+
+	length = 0;
+	maxLength = 0;
+
+	for(int k = i2; k <= j2; ++k) {
+	  length = 0;
+	  int k2 = k;
+	  while(k2 != 1) {
+		if(k2 % 2 == 1) {
+		  k2 = k2 * 3 + 1;
+		  length++;
+		} else {
+		  k2 /= 2;
+		  length++;
+		}
+	  }
+	  length++;
+	  if(length > maxLength) maxLength = length;
+	}
+	std::cout << i << " " << j << " " << maxLength << "\n";
+  }
+
+  return 0;	
 }
